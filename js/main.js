e1610204a234d011230c5ee4628e5f5f9374a5e3
sunri5e <sunrise944@gmail.com>
2015-10-06 14:34:57 +0300

    added pop-up

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ $(document).ready(function(){
   $('.gl-slider').bxSlider({
   	controls: true
   });
+
   $('.gl-vacancies-slider').bxSlider({
   	controls: true,
   	slideWidth: 240,
@@ -10,5 +11,25 @@ $(document).ready(function(){
     moveSlides: 1,
     slideMargin: 65,
     pager: false
+  });
+
+  $(".gl-slide-desc a").on("click", function(e){
+    e.preventDefault();
+    $($(this).attr('href')).fadeIn();
+    $("body").css("overflow","hidden");
+  });
+
+  $(".pop-up-close").on("click", function(e){
+    e.preventDefault();
+    $(this).parents(".gl-post-popup").fadeOut().removeClass("open");
+    $("body").css("overflow","auto");
+  });
+
+  $(".gl-main-nav a").on("click", function(e) {
+    e.preventDefault();
+    var blockOffset = $($(this).attr('href')).offset().top;
+    $('body,html').animate({scrollTop: blockOffset}, 800);
+    // $(".gl-main-nav a").removeClass("active");
+    // $(this).addClass("active");
   });
 });
